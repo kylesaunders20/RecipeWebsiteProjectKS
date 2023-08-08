bdf7df06ae2512d1f9280794c66e9b4bd41787e5
Kylesaunders20 <kyle_saunders20@hotmail.com>
2023-08-08 04:23:21 -0230

    Styling and Theming:
    Integrated Bootstrap to give a consistent and professional look to the website.
    Customized the Bootstrap theme using a specific color palette and typography.
    Recipe Gallery Enhancements:
    
    Implemented the ability to fetch recipes from a CSV file.
    Displayed the recipes in the gallery, complete with images, preparation instructions, and serving suggestions.
    Implemented a recipe rating system that calculates and displays the average rating for each recipe.
    Recipe Submission:
    
    Enhanced the recipe submission form to include all necessary fields.
    Allowed users to submit recipes, which are then stored in the CSV file.
    Implemented image upload functionality so that users can provide an image for their recipe.
    Recipe Management:
    
    Displayed all existing recipes in a list format.
    Provided a "Delete" button next to each recipe, enabling users to remove recipes from the list.
    Implemented backend functionality to handle the deletion of recipes and update the CSV file accordingly.
    Enhanced Error Handling:
    
    Made adjustments to handle various potential errors, such as issues with reading from the CSV file or handling user inputs.
    Miscellaneous Improvements:
    
    Made various tweaks and optimizations to the code for better performance and maintainability.
    Addressed specific issues and bugs that arose during development, ensuring a smooth user experience.

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ def index():
 @app.route('/gallery')
 def gallery():
     recipes = fetch_recipes()
+    for recipe in recipes:
+        ratings = [int(rating) for rating in recipe["user_ratings"].split(",") if rating]
+        recipe["average_rating"] = sum(ratings) / len(ratings) if ratings else "No ratings yet"
     return render_template('gallery.html', recipes=recipes)
 
 
@@ -38,27 +41,100 @@ def submit():
 
 @app.route('/manage')
 def manage():
-    return render_template('manage.html')
+    recipes = fetch_recipes()
+    return render_template('manage.html', recipes=recipes)
+
 
 
 @app.route('/search', methods=['GET'])
 def search():
-    # We'll handle the search logic later.
-    # For now, just render the search page.
-    return render_template('search.html')
+    search_term = request.args.get('search_term')
+    ingredient_filter = request.args.get('ingredient_filter')
+    recipes = search_recipes(search_term, ingredient_filter)
+    return render_template('search.html', recipes=recipes, search_term=search_term, ingredient_filter=ingredient_filter)
+
+@app.route('/rate_recipe', methods=['POST'])
+def rate_recipe():
+    recipe_name = request.form.get("recipe_name")
+    rating = request.form.get("rating")
+    if recipe_name and rating:
+        update_ratings(recipe_name, rating)
+    return redirect(url_for('gallery'))  # Redirect back to gallery after rating
+
+
+@app.route('/delete_recipe/<recipe_name>', methods=['POST'])
+def delete_recipe(recipe_name):
+    recipes = fetch_recipes()
+    recipes = [recipe for recipe in recipes if recipe['recipe_name'] != recipe_name]
+
+    # Save the updated recipes back to the CSV
+    with open(os.path.join("csvfiles", "recipes.csv"), "w", newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=["recipe_name", "image_link", "ingredients", "preparation", "serving",
+                                                  "user_ratings"])
+        writer.writeheader()
+        for recipe in recipes:
+            writer.writerow(recipe)
+
+    return redirect(url_for('manage'))
+
 
 def fetch_recipes():
     recipes = []
-    with open(os.path.join("data", "recipes.csv"), "r") as file:
+    with open(os.path.join("csvfiles", "recipes.csv"), "r") as file:
         reader = csv.DictReader(file)
         for row in reader:
             recipes.append(row)
     return recipes
 
 def save_recipe(recipe_data):
-    with open(os.path.join("data", "recipes.csv"), "a", newline='') as file:
+    with open(os.path.join("csvfiles", "recipes.csv"), "a", newline='') as file:
         writer = csv.DictWriter(file, fieldnames=["recipe_name", "image_link", "ingredients", "preparation", "serving", "user_ratings"])
         writer.writerow(recipe_data)
+
+
+def search_recipes(search_term=None, ingredient_filter=None):
+    recipes = fetch_recipes()
+    filtered_recipes = []
+
+    for recipe in recipes:
+        if search_term and search_term.lower() not in recipe['recipe_name'].lower():
+            continue
+        if ingredient_filter and ingredient_filter.lower() not in recipe['ingredients'].lower():
+            continue
+        filtered_recipes.append(recipe)
+
+    return filtered_recipes
+
+
+def get_ratings(recipe_name):
+    recipes = fetch_recipes()
+    for recipe in recipes:
+        if recipe["recipe_name"] == recipe_name:
+            return [int(rating) for rating in recipe["user_ratings"].split(",") if rating]
+    return []
+
+
+def update_ratings(recipe_name, new_rating):
+    recipes = fetch_recipes()
+    updated = False
+
+    # Find the recipe and update its ratings
+    for recipe in recipes:
+        if recipe["recipe_name"] == recipe_name:
+            current_ratings = recipe["user_ratings"].split(",")
+            current_ratings.append(str(new_rating))
+            recipe["user_ratings"] = ",".join(current_ratings)
+            updated = True
+            break
+
+    # If we updated the ratings, save back to the CSV
+    if updated:
+        with open(os.path.join("csvfiles", "recipes.csv"), "w", newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=["recipe_name", "image_link", "ingredients", "preparation", "serving", "user_ratings"])
+            writer.writeheader()
+            for recipe in recipes:
+                writer.writerow(recipe)
+
 
 
 if __name__ == '__main__':
